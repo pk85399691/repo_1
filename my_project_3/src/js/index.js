510f3328b29a0ab4ba7e6a4db179b993fb288918
pk85399691 <pk853996@gmail.com>
2020-06-05 00:25:32 +0530

    first commit project 3

--- my_project_3/src/js/index.js
+++ my_project_3/src/js/index.js
@@ -1,13 +1,20 @@
-
-
 import * as reciever from './reciever'
 reciever.default.getData();
 
 
+const signup = () => document.getElementById('_upload').addEventListener('click',()=>{
+    signupHandler();
+})
+const signupHandler=()=>{
+    document.querySelector('.modal').style.display="block";
+    document.querySelector('.close').addEventListener('click',()=>{
+        document.querySelector('.modal').style.display="none";  
+    })
+    document.querySelector('body').style.opacity=0.6;
+    console.log('signup');
+}
 
-
-
-
+signup();
 
 
 
